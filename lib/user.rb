b0bcfc6e7069ca58d229ae26adf9b5e2e7d0237b
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-03-11 04:12:04 -0300

    .kind_of? --> .is_a?

--- lib/user.rb
+++ lib/user.rb
@@ -12,7 +12,7 @@ class User
                 :hireable, :id
 
   def initialize(params)
-    if params.kind_of? String or params.kind_of? Symbol
+    if params.is_a? String or params.is_a? Symbol
       hash = HTTParty.get "#{BASE_URL}users/#{params}"
       hash.each { |attr, value| __send__("#{attr}=", value)}
     else
