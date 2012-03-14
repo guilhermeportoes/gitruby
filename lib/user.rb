71d2c51bc3a229d6c706dfb861f301b7f285b679
Rodrigo Manhães <rmanhaes@gmail.com>
2012-03-14 15:37:59 -0300

    refactoring: dynamic attr_accessor

--- lib/user.rb
+++ lib/user.rb
@@ -3,20 +3,18 @@ require 'httparty'
 class User
   BASE_URL = 'https://api.github.com/'
 
-  # TODO: generate attr_accessor dinamically
-  attr_accessor :type, :following, :followers, :html_url, :bio, :avatar_url,
-                :login, :public_gists, :created_at, :location, :blog,
-                :name, :company, :email, :url, :gravatar_id, :public_repos,
-                :hireable, :id
-
   def initialize(params)
     if params.is_a? String or params.is_a? Symbol
       params = HTTParty.get "#{BASE_URL}users/#{params}"
     end
-    params.each { |attr, value| __send__("#{attr}=", value)}
+    params.each do |attr, value|
+      self.class.send(:attr_accessor, attr)
+      send("#{attr}=", value)
+    end
   end
 
   def self.find(username)
     new(HTTParty.get "#{BASE_URL}users/#{username}")
   end
 end
+
